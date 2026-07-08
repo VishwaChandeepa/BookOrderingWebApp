800b8cc08848bcb1a64b4eb2e73c84992f13c072
VishwaChandeepa <chandeepavishwa@gmail.com>
2026-07-08 22:42:00 +0530

    Prepare backend for Azure deployment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 
-console.log("User:", process.env.DB_USER);
-console.log("Password:", process.env.DB_PASSWORD);
-
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
